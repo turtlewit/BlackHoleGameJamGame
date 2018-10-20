5f710f1bc5e44e2fea46b7f43ba82b392bd2ab7e
turtlewit <turtlewit@gmail.com>
2018-10-20 14:15:04 -0700

    add generic entity

---
+++ Scripts/Entities/IntractableEntity.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var can_interact = false
+onready var interaction_key = get_node("/root/Manager").interaction_key
+onready var interaction_key_letter = get_node("/root/Manager").interaction_key_letter
+
+export(NodePath) var entity_popup_path = NodePath("EntityPopup")
+export(NodePath) var entity_popup_key_path = NodePath("EntityPopup/Key")
+onready var entity_popup = get_node(entity_popup_path)
+onready var entity_popup_key = get_node(entity_popup_key_path)
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	entity_popup_key.text = interaction_key_letter
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	if can_interact:
+		if Input.is_action_just_pressed(interaction_key):
+			on_interaction()
+
+func on_interaction():
+	pass
+
+func _on_body_entered(body):
+	can_interact = true
+	print("true")
+	entity_popup.visible = true
+
+
+func _on_body_exited(body):
+	can_interact = false
+	entity_popup.visible = false
