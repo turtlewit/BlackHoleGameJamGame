eedbab203c8b440ae2fd6ec1220811022f8ca35c
turtlewit <turtlewit@gmail.com>
2018-10-20 15:07:13 -0700

    Add door entity

---
+++ DoorEntity.gd
@@ -0,0 +1,22 @@
+extends "res://Scripts/Entities/IntractableEntity.gd"
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+onready var door = get_node("DoorBody")
+onready var manager = get_node("/root/Manager")
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func on_interaction():
+	manager.time -= 60
+	door.visible = false
