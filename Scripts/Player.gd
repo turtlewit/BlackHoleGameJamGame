7fa06e67d4b41b3167e4da7d08fe092adb3eb018
Lillian <lilliw2@uci.edu>
2018-10-21 15:25:03 -0700

    add mouse-facing player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,26 +3,23 @@ extends KinematicBody2D
 var speed = 300
 var velocity = Vector2()
 
-onready var sprite = $PlayerSprite
-
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed('move_right'):
+	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
-		sprite.set_rotation_degrees(270)
-	if Input.is_action_pressed('move_left'):
+		set_rotation_degrees(270)
+	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
-		sprite.set_rotation_degrees(90)
-	if Input.is_action_pressed('move_down'):
+		set_rotation_degrees(90)
+	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
-		sprite.set_rotation_degrees(0)
-	if Input.is_action_pressed('move_up'):
+		set_rotation_degrees(0)
+	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
-		sprite.set_rotation_degrees(180)
+		set_rotation_degrees(180)
 	velocity = velocity.normalized() * speed
 
-func _process(delta):
-	get_input()
-
 func _physics_process(delta):
-	move_and_collide(velocity * delta)
+	get_input()
+	velocity = move_and_collide(velocity * delta)
+		
